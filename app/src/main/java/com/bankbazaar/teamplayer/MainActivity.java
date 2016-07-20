dcc781f4847641100cff7fe4b3fcdc5c12cabedb
Karan Chugh <karan.chugh@bankbazaar.com>
2016-07-20 14:16:24 +0530

    Initial DAM

---
+++ app/src/main/java/com/bankbazaar/teamplayer/MainActivity.java
@@ -0,0 +1,36 @@
+package com.bankbazaar.teamplayer;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.capture_button);
+        button.setOnClickListener(this);
+//        dispatchTakePictureIntent();
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        dispatchTakePictureIntent();
+    }
+}
